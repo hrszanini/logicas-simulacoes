dde6c132f3732dc117a84e1a7d2a19df891039bf
hrszanini <hrszanini@gmail.com>
2022-12-03 20:56:24 -0300

    Adição de quadtree e pastas

---
+++ quadtree/scripts/parameters.js
@@ -0,0 +1,9 @@
+// Parâmetros lógicos
+var POINTS_QUANTITY = 20;
+var QUADTREE_CAPACITY = 1;
+
+// Parâmetros visuais
+var POINT_SIZE = 5;
+var POINT_COLOR = 'red';
+var QUADTREE_COLOR = 'white';
+var QUADTREE_DRAW_DELAY_IN_MILI = 0;
